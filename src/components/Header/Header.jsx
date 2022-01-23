4a981a164a5528c33850676ec650213bfad2b734
Anton Merkotun <antonmerkotun@gmail.com>
2022-01-23 23:42:29 +0200

    start header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,22 @@ function Header() {
     return (
         <div className="header">
             <div className="header__container">
-                <a className="header__logo"><img className="header__logo-img" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" alt="header-logo"/></a>
-                <div>input</div>
-                <div>icon</div>
+                <a className="header__logo">
+                    <img className="header__logo-img"
+                         src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+                         alt="header-logo"/>
+                </a>
+                <div className="header__search">
+                    <input className="header__search-input" type="text" placeholder="Поиск"/>
+                </div>
+                <div className="header__nav">
+                    <span>icon</span>
+                    <span>icon</span>
+                    <span>icon</span>
+                    <span>icon</span>
+                    <span>icon</span>
+                    <span>icon</span>
+                </div>
             </div>
         </div>
     );
