d929c420ce73d31f7b9220c01b93fe4b436ac092
Anton <antonmerkotun@gmail.com>
2022-01-25 17:55:35 +0200

    start main block

---
+++ src/components/Recommendation/Recommendation.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+//style
+import "./Recommendation.scss"
+
+const Recommendation = () => {
+    return (
+        <div className='recommendation'>
+            <div>Пользователь</div>
+            <div>Рекомендации</div>
+            <div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+            </div>
+            <div>
+                <p>Информация Помощь Пресса API Вакансии</p>
+                <p>Конфиденциальность Условия Места</p>
+                <p>Популярные аккаунты Хэштеги Язык </p>
+            </div>
+            <div>© 2022 INSTAGRAM FROM META</div>
+        </div>
+    );
+};
+
+export default Recommendation;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,6 +5,7 @@ import "./Main.scss"
 
 //components
 import PostList from "../../components/PostList/PostList";
+import Recommendation from "../../components/Recommendation/Recommendation";
 
 
 
@@ -15,7 +16,8 @@ const Main = () => {
                 <PostList/>
             </div>
             <div className="list-people">
-                <div className="list-people-fixed">fix
+                <div className="list-people-fixed">
+                    <Recommendation/>
                 </div>
             </div>
         </div>
