464b2e3ff16ee6d0fedaa6c84cf546ce8f563495
Anton <antonmerkotun@gmail.com>
2022-02-09 13:08:26 +0200

    server

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const {MongoClient, ObjectId} = require('mongodb')
-const bodyParser = require('body-parser')
+const {MongoClient, ObjectId} = require('mongodb');
+const bodyParser = require('body-parser');
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
 const url = "mongodb+srv://antonmerkotun:1996178dD@instagram.9subi.mongodb.net/insta?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 const PORT = 3005
 
 client.connect(err => {
-
-///////////////////////////////GET
     const usersCollections = client.db("instagram").collection("users");
+    const accountCollections = client.db("instagram").collection("account");
 
+//GET
 
+//User
     app.get('/users', async function (req, res) {
         const allUsers = await usersCollections.find({}).toArray();
         res.send(allUsers);
@@ -26,8 +27,6 @@ client.connect(err => {
         res.send(user);
     })
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     app.get('/user/:id/posts', async function (req, res) {
         const id = req.params.id
         const user = await usersCollections.find({_id: ObjectId(id)}).toArray();
@@ -36,7 +35,6 @@ client.connect(err => {
         })
         res.send(posts);
     })
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     app.get('/post/:id', async function (req, res) {
         const id = req.params.id
@@ -52,38 +50,39 @@ client.connect(err => {
         res.send(post);
     })
 
-
-//////////////////////////
-
-
-    const accountCollections = client.db("instagram").collection("account");
+//Account
     app.get('/account', async function (req, res) {
         const findResult = await accountCollections.find({}).toArray();
 
         res.send(findResult);
     })
 
-////////////////////////////////
+//POST
 
 
-    // const userPosts = client.db("instagram").collection("users");
-    // app.post('/users/post/:id', async function (req, res) {
-    //     const id = req.params.id
-    //
-    //     const findResult = await userPosts.find({_id: ObjectId(id)}).toArray();
-    //
-    //     res.send(findResult);
-    // })
+    app.post('/post/:id', async function (req, res) {
+        const userData = req.body
+        console.log(userData)
 
-    // app.post('/users', async function (req, res) {
-    //     const userData = req.body
-    //
-    //     const insertResult = await usersCollections.insertMany([userData]);
-    //
-    //     res.sendStatus(200)
-    // })
+        // const insertResult = await usersCollections.insertMany([userData]);
+
+
+        res.sendStatus(200)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
 
     app.listen(PORT, () => {
-        console.log("Connected server instagram")
+        console.log(`Connected server to port ${PORT}`)
     });
 });
