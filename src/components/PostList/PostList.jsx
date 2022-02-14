78c7f865e22ba9c3f042f276efa8ba5b226e6bfd
Anton <antonmerkotun@gmail.com>
2022-02-14 17:02:38 +0200

    add reducer posts and comments

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -12,31 +12,42 @@ import Loading from "../Loading/Loading";
 
 
 const PostList = ({usersData, postsData, commentsData}) => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        usersData.data.forEach(users => {
+            postsData.data.forEach(posts => {
+                if (users._id === posts.user) {
+                    setPosts(posts)
+                }
+            })
+        })
+    }, [usersData, postsData, commentsData])
 
 
     return (
         <div className="post-list">
-            {console.log(commentsData)}
-            {/*{postsData.error === "Unexpected token P in JSON at position 0" && "Произошла ошибка! Проверьте подключение к базе данных MongoDB."}*/}
+
+            {postsData.error === "Unexpected token P in JSON at position 0" && "Произошла ошибка! Проверьте подключение к базе данных MongoDB."}
             {/*{postsData.loading === true ? <Loading/> : <>*/}
             {/*    {postsData.data === undefined ? <p>нету подписок</p> : <>*/}
-                    {/*{accountData.account.subscriptions.length === 0 ? '' : accountData.account.subscriptions.map((user) => {*/}
-                    {/*        return accountData.account.subscriptions.map(post => {*/}
-                    {/*            return <li className="post-list-block" key={post._id}>*/}
-                    {/*                <Post*/}
-                    {/*                    nickName={user.nickName}*/}
-                    {/*                    lastName={user.lastName}*/}
-                    {/*                    avatar={user.avatar}*/}
-                    {/*                    post={post.img}*/}
-                    {/*                    comments={post.comments}*/}
-                    {/*                    favoriteNumber={post.comments.length}*/}
-                    {/*                />*/}
-                    {/*            </li>*/}
-                    {/*        })*/}
-                    {/*    }*/}
-                    {/*)}*/}
-                {/*</>*/}
-                {/*}*/}
+            {/*{accountData.account.subscriptions.length === 0 ? '' : accountData.account.subscriptions.map((user) => {*/}
+            {/*        return accountData.account.subscriptions.map(post => {*/}
+            {/*            return <li className="post-list-block" key={post._id}>*/}
+            {/*                <Post*/}
+            {/*                    nickName={user.nickName}*/}
+            {/*                    lastName={user.lastName}*/}
+            {/*                    avatar={user.avatar}*/}
+            {/*                    post={post.img}*/}
+            {/*                    comments={post.comments}*/}
+            {/*                    favoriteNumber={post.comments.length}*/}
+            {/*                />*/}
+            {/*            </li>*/}
+            {/*        })*/}
+            {/*    }*/}
+            {/*)}*/}
+            {/*</>*/}
+            {/*}*/}
             {/*</>*/}
             {/*}*/}
         </div>
