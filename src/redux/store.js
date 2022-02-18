8eaad9a93281ef8af2d1bdf65bb8f7e36f80edd1
Anton Merkotun <antonmerkotun@gmail.com>
2022-02-19 00:04:27 +0200

    make it better

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,19 @@
 import {applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "./rootReducer";
 import thunk from "redux-thunk";
+import {routerMiddleware} from 'react-router-redux'
 
 
-export const store = createStore(rootReducer, compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+// export const store = createStore(rootReducer, compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ))
+
+export const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.navigator.userAgent.includes('Chrome') ?
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
+    ),
+);
